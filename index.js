b71afb0b42a3976f6ec912c48cb772a913e9e357
atul <patelatul948@gmail.com>
2024-05-21 12:59:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './api/routes/user.route.js';
+import authRouter from './api/routes/auth.route.js';
+import listingRouter from './api/routes/listing.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+dotenv.config();
+const url=process.env.URL
+const port=process.env.PORT
+
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log('dataabse connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
